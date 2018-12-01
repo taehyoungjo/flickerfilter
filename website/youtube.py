e48fbcca54757da417a0884d77b7ff78d868a95c
Tae Hyoung Jo <taehyoung.jo@gmail.com>
2018-12-01 16:55:23 -0500

    youtube_dl video only lowest quality

--- website/youtube.py
+++ website/youtube.py
@@ -2,7 +2,9 @@ from __future__ import unicode_literals
 import youtube_dl
 
 def download(str):
-    ydl_opts = {}
+    ydl_opts = {
+        'format': '160'
+    }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([str])
     return True
