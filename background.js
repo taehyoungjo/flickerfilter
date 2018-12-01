da42e738eba55e111d792927b0e559913b745546
John Kim <johnk.395@gmail.com>
2018-12-01 03:17:24 -0500

    Initial Commit
    Created manifest, background.js.
    
    Extension only enabled when video detected on www.youtube.com host.

---
+++ background.js
@@ -0,0 +1,15 @@
+let rule1 = {
+	conditions: [
+		new chrome.declarativeContent.PageStateMatcher({
+			pageUrl: { hostEquals: 'www.youtube.com', schemes: ['https'] },
+			css: ["video"]
+		})
+	],
+	actions: [ new chrome.declarativeContent.ShowPageAction() ]
+};
+
+chrome.runtime.onInstalled.addListener(function() {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		chrome.declarativeContent.onPageChanged.addRules([rule1]);
+	});
+});
